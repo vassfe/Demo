d03a8f8ac7faa9b6aba410feb2973c722bf47c04
Vasilis Sfetsios <vassfe@gmail.com>
2022-07-31 17:49:31 +0200

    Printing variables

--- brewery.py
+++ brewery.py
@@ -1,4 +1,11 @@
-print("The name of your beer \n is ' " + input("What city would you like to visit?: ") + " " + input("What is your fav color?: ") + "'\n Congratulations")
+# print("The name of your beer \n is ' " + input("What city would you like to visit?: ") + " " + input("What is your fav color?: ") + "'\n Congratulations")
 
+name = 'Vasilis'
+print('My name is ' + name + ' and I am a hooman')
+print('My name is ' + name)
+
+age = 28
+print(f'My age is {age}')
+print('My age is {}'.format(age))
 
 
